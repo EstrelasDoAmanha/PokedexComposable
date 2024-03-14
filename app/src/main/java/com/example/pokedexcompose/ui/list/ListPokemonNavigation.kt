3428c69d27601828b8f9ae2f43be20061cbabf96
Thailan Godoy <thailanrg@gmail.com>
2024-03-14 14:42:44 -0300

    adjusting named

--- app/src/main/java/com/example/pokedexcompose/ui/list/ListPokemonNavigation.kt
+++ app/src/main/java/com/example/pokedexcompose/ui/list/ListPokemonNavigation.kt
@@ -1,13 +1,7 @@
 package com.example.pokedexcompose.ui.list
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
@@ -25,7 +19,7 @@ fun NavGraphBuilder.pokemonList(
     }
 }
 
-fun NavHostController.navigateToDrawTeams(
+fun NavHostController.navigateToPokemonList(
     navOptions: NavOptions? = null
 ) {
     navigate(pokemonListRoute, navOptions)
