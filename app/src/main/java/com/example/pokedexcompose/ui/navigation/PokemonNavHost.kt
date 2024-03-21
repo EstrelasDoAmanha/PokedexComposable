25b24d917b0e0a516cee6f0e7f82279c2f6df5e1
gabriel-estrela <gabrielestrela92@gmail.com>
2024-03-21 14:56:35 -0300

    ktlint: fix style

--- app/src/main/java/com/example/pokedexcompose/ui/navigation/PokemonNavHost.kt
+++ app/src/main/java/com/example/pokedexcompose/ui/navigation/PokemonNavHost.kt
@@ -3,8 +3,8 @@ package com.example.pokedexcompose.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.example.pokedexcompose.ui.list.pokemonList
 import com.example.pokedexcompose.ui.list.POKEMON_LIST_ROUTE
+import com.example.pokedexcompose.ui.list.pokemonList
 
 @Composable
 fun PokemonNavHost(navController: NavHostController) {
